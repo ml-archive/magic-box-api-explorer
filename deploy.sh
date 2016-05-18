27eac1893ee43749e8b5ebbc1a29c3c6f6b98ee4
Simon Yousoufov <syousoufov@fuzzproductions.com>
2016-05-18 10:28:24 -0400

    Point deploy script to correct S3 bucket, lower top padding for nav and content containers

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ npm run dist
 
 if [ "$1" = "prod" ]
 then
-    BUCKET="simonyo.us"
+    BUCKET="explorer.magic-box.fuzzpro.com"
 else
     BUCKET="${1}"
 fi
@@ -13,4 +13,4 @@ fi
 echo "Deploying to ${BUCKET}"
 
 ## Deploy to AWS
-aws s3 sync dist/ s3://${BUCKET} --profile simonyous --acl public-read
+aws s3 sync dist/ s3://${BUCKET} --profile crub --acl public-read
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,7 @@ class NavComponent extends React.Component {
 	render() {
 		return (
 			<nav className="navbar no-margin-bottom alt-font bg-white sidebar-nav">
-				<div className="container navigation-menu">
+				<div className="container navigation-menu main-nav-container">
 					<div className="row">
 						<div className="col-md-12 col-sm-12 navbar-header margin-twenty no-margin-lr no-margin-top sm-no-margin">
 							<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
